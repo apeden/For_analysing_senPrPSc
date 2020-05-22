1ef5dc77429138c169f99b59cd66add49d3b8871
apeden <apeden23@gmail.com>
2020-05-22 08:59:52 +0100

    now includes a function for extracting mean and std N, D and D-N vals from an array of calibrated CDI vals, putting these in a dataframe indexed by column, numbered from 0

--- Fig8.py
+++ Fig8.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import math
 
-toPrt = False
+toPrt = True
 
 def to_numpy(file_name, sheet_name):
     """converts CDI data excel file to numpy array""" 
@@ -21,14 +21,14 @@ def substrate_blanks(numparr, *blank_rows):
     if toPrt: print("Numpy array minus blanks\n=======\n",numparr)
     return numparr
 
-def std_curve(numparr, curve_start_col):
+def std_curve(numparr, curve_start_col, curve_start = 0, curve_end = 4):
     """CDI standard curve coeff and intercept on y axis"""
-    X = np.array([0.25, 0.5, 0.75, 1])
-    y = np.array([numparr[4:7, curve_start_col+1],
+    X = np.array([0.25, 0.5, 0.75, 1][curve_start - 1:curve_end])
+    curve_points = [numparr[4:7, curve_start_col+1],
                   numparr[4:7, curve_start_col],
                   numparr[1:4, curve_start_col+1],
-                  numparr[1:4, curve_start_col]],
-                 dtype = float)
+                  numparr[1:4, curve_start_col]][curve_start - 1:curve_end]
+    y = np.array(curve_points,dtype = float)
     if toPrt: print("y vals for standard curve\n=======\n",y)
     result = np.polyfit(X,y,1)
     coeff = result[0].mean()
@@ -37,7 +37,7 @@ def std_curve(numparr, curve_start_col):
     if toPrt: print("Intercept on y axis =", str(intercept))
     return coeff, intercept
 
-def relate_to_PrP(numparr, coeff, intercept, ul_analysed = 10):
+def relate_to_PrP(numparr, coeff, intercept, ul_analysed = 25):
     """convert CDI values to PrP per g brain, using std curve"""
     numparr = (numparr[1:7,0:10]-intercept)/coeff
     if toPrt: print("Values as ug PrP per 2.5mg brain sample\n========\n",numparr)
@@ -45,19 +45,18 @@ def relate_to_PrP(numparr, coeff, intercept, ul_analysed = 10):
     if toPrt: print("Values as ug PrP per gram brain \n========\n",numparr)
     return numparr
 
-def get_vals(file_name, sheet_name, curve_col_start):
+def get_vals(file_name, sheet_name, curve_col_start, curve_start = 1, curve_end =4):
     """gets data and converts CDI vals to concentrations of PrP
     Returns as numpy array 
     """
     numparr = to_numpy(file_name, sheet_name)
     numparr = substrate_blanks(numparr, 0,7)
-    coeff, intercept = std_curve(numparr, curve_col_start)
+    coeff, intercept = std_curve(numparr, curve_col_start, curve_start, curve_end)
     numparr = relate_to_PrP(numparr, coeff, intercept)
     return numparr
-    
-        
+         
 def means_SDs(vals):
-    """takes array of values and returns arrays of (i) means and (ii) sds"""
+    """takes array of values and returns mean and sd"""
     vals = np.array(vals, dtype=np.float64)
     means = np.mean(vals, axis=0)
     SDs = np.std(vals, axis=0)
@@ -86,76 +85,113 @@ def plotBar(title, mean_vals, DNstd, PK_concs = ('0', '1', '2.5', '10', '50')):
         y_scale += gradation
     plt.yticks(np.arange(0, y_scale, gradation))
 
-#get data into numpy arrays
+##############  get data into numpy arrays   ######################
 #numparrPMCA = get_vals("CDI13 030B.xls", "Plate", curve_col_start = 10)
 numparrFFIoneP1 = get_vals("CDI13 004.xls", "Plate", curve_col_start = 9)
-numparrFFIoneP2 = get_vals("CDI13 004b.xls", "Plate", curve_col_start = 9)
-numparrvCJDFC= get_vals("CDI 12 007 PLATE B.xls", "Plate", curve_col_start = 10)
-##numparrvCJDthal= get_vals("CDI 12 016 PLATE B.xls", "Plate", curve_col_start = 9)
+#numparrFFIoneP2 = get_vals("CDI13 004b.xls", "Plate", curve_col_start = 9)
+#numparrvCJDFC= get_vals("CDI 12 007 PLATE B.xls", "Plate", curve_col_start = 10)
+##numparrFFIoneThal= get_vals("CDI 12 016 PLATE A.xls", "Plate", curve_col_start = 9)
+##numparrvCJDthal= get_vals("CDI 12 016 PLATE B.xls", "Plate", curve_col_start = 9, curve_start = 1, curve_end =3)
 ##numparrsCJDthal= get_vals("CDI13 004b.xls", "Plate", curve_col_start = 6)
 ##numparrsCJDMM2TFC= get_vals("CDI 12 008 PLATE B.xls", "Plate", curve_col_start = 10)
 ##numparrsCJDMM2CFC= get_vals("CDI 12 009 PLATE A.xls","Plate", curve_col_start = 10)
-numparrNonCJDone = get_vals("CDI13 007.xls","Plate", curve_col_start = 10)
-numparrNonCJDtwo = get_vals("CDI13 007b.xls","Plate", curve_col_start = 10)
+##numparrNonCJDone = get_vals("CDI13 007.xls","Plate", curve_col_start = 10)
+##numparrNonCJDtwo = get_vals("CDI13 007b.xls","Plate", curve_col_start = 10)
 
+def getD_N_vals(arr, first_col,last_col ):
+    return arr[3:,first_col:last_col+1]-arr[0:3,first_col:last_col+1] 
 
-#calculate D-N vals
-##DN_vals_PMCA = numparrPMCA[3:,0:5] - numparrPMCA[0:3,0:5]
-##DN_vals_PMCA2 = numparrPMCA[3:,5:10] - numparrPMCA[0:3,5:10]
-DN_vals_case1 = numparrFFIoneP1[3:,6:9] - numparrFFIoneP1[0:3,6:9]
-DN_vals_case1 = np.concatenate((DN_vals_case1,(numparrFFIoneP2[3:,0:3] - numparrFFIoneP2[0:3,0:3])), axis = 1) #had to glue data from separate plates
-DN_vals_case2 = numparrFFIoneP2[3:,3:9] - numparrFFIoneP2[0:3,3:9]
-##DN_vals_vCJDthal = numparrvCJDthal[3:,3:9] - numparrvCJDthal[0:3,3:9]
-#DN_vals_vCJDFC = numparrvCJDFC[3:,:10] - numparrvCJDFC[0:3,:10]###have to change vol analysed to 10ul!####
-DN_vals_sCJDMM1FC = numparrFFIoneP1[3:,:6] - numparrFFIoneP1[0:3,:6]
-##DN_vals_sCJDMM1Th = numparrsCJDthal[3:,:6] - numparrsCJDthal[0:3,:6]
-##DN_vals_sCJDMM2TFC = numparrsCJDMM2TFC[3:,:10] - numparrsCJDMM2TFC[0:3,:10]
-##DN_vals_sCJDMM2CFC = numparrsCJDMM2CFC[3:,:10] - numparrsCJDMM2CFC[0:3,:10]
-DN_vals_58 = numparrNonCJDone[3:,0:5] - numparrNonCJDone[0:3,0:5]
-DN_vals_39 = numparrNonCJDone[3:,5:10] - numparrNonCJDone[0:3,5:10]
-DN_vals_54 = numparrNonCJDtwo[3:,0:5] - numparrNonCJDtwo[0:3,0:5]
-DN_vals_65 = numparrNonCJDtwo[3:,5:10] - numparrNonCJDtwo[0:3,5:10]
+##DN_vals_PMCA        = getD_N_vals(numparrPMCA,          0, 4)
+##DN_vals_PMCA        = getD_N_vals(numparrPMCA,          5, 9)
+#DN_vals_case1       = getD_N_vals(numparrFFIoneP1,      8, 8)
+#DN_vals_case1       = np.concatenate((DN_vals_case1,getD_N_vals(numparrFFIoneP2, 0, 2)), axis = 1) #had to glue data from separate plates
+##DN_vals_case2       = getD_N_vals(numparrFFIoneP2,      5, 8)
+##DN_vals_FFIoneThal  = getD_N_vals(numparrFFIoneThal,    0, 5)
+##DN_vals_FFItwoThal  = getD_N_vals(numparrFFIoneThal,    6, 8)
+##DN_vals_FFItwoThal  = np.concatenate((DN_vals_FFItwoThal, getD_N_vals(numparrvCJDthal, 0, 2)), axis = 1) #had to glue data from separate plates
+##DN_vals_vCJDthal    = getD_N_vals(numparrvCJDthal,      3, 8)
+##DN_vals_vCJDFC      = getD_N_vals(numparrvCJDFC,        0, 9)###have to change vol analysed to 10ul!####
+##DN_vals_sCJDMM1FC   = getD_N_vals(numparrFFIoneP1,      2, 5)
+##DN_vals_sCJDMM1Th   = getD_N_vals(numparrsCJDthal,      0, 5)
+##DN_vals_sCJDMM2TFC  = getD_N_vals(numparrsCJDMM2TFC,    0, 9)
+##DN_vals_sCJDMM2CFC  = getD_N_vals(numparrsCJDMM2CFC,    0, 9)
+##DN_vals_58          = getD_N_vals(numparrNonCJDone,     2, 4)
+##DN_vals_39          = getD_N_vals(numparrNonCJDone,     7, 9)
+##DN_vals_54          = getD_N_vals(numparrNonCJDtwo,     2, 4)
+##DN_vals_65          = getD_N_vals(numparrNonCJDtwo,     7, 9)
+##DN_nonCJD   = DN_vals_58, DN_vals_39, DN_vals_54, DN_vals_65
 
-DN_nonCJD = DN_vals_58, DN_vals_39, DN_vals_54, DN_vals_65 
+def printResSen(DN_array, label, pos2_5 = 2, pos50 = 5): 
+    """for printing means and std for:
+    total PrPSc (that remaining after 2.5ug/ml PK)
+    res PrPSc   (that remaining after 50ug/ml  PK)
+    sen PrPSc   (total - res PrPSc)
 
-def getResSen(val_array, label, pos2_5 = 2, pos50 = 5): 
-    print("D-N values for ", label, val_array)
-    totMean, totSD = means_SDs(val_array[:,pos2_5])
+    DN_array is an array of calibrated D-N values.
+    pos2_5 = 2, pos50 = 5 are the positions of the\
+    columns containing samples treated with 2.5ug/ml PK\
+    and 50ug/ml PK, respectively
+    """
+    print("D-N values for ", label, DN_array)
+    totMean, totSD = means_SDs(DN_array[:,pos2_5])
     print("Mean tot val and std ",label,":", totMean, totSD)
-    resMean, resSD = means_SDs(val_array[:,pos50])
+    resMean, resSD = means_SDs(DN_array[:,pos50])
     print("Mean res val and std ",label,":", resMean, resSD)
-    senMean, senSD = means_SDs(val_array[:,pos2_5]-val_array[:,pos50])
+    senMean, senSD = means_SDs(DN_array[:,pos2_5]-DN_array[:,pos50])
     print("Mean sen val and std ",label,":", senMean, senSD)
-#getResSen(DN_vals_vCJDFC, "vCJD FC", pos2_5 = 5, pos50 = 8)
 
-#get values for plotting
+
+##printResSen(DN_vals_case1, "FFI case 1")
+##printResSen(DN_vals_FFItwoThal, "FFI case 2 Thal")
+
+def _PrPC_PrPSc(val_array, first_col, last_col):
+    """calculates mean/std N value, D value, D-N value\
+    for each column in range, returns result as a dataframe
+    """
+    val_array = np.array(val_array, dtype=np.float64)
+    data_dict  = {"label":["N","Nstd","D","Dstd","D-N","D-Nstd"]}
+    for column in range(first_col, last_col+1):
+        data_list = []
+        data_list.append(np.mean(val_array[0:3,column]))
+        data_list.append(np.std(val_array[0:3,column]))
+        data_list.append(np.mean(val_array[3:,column]))
+        data_list.append(np.std(val_array[3:,column]))
+        data_list.append(np.mean(val_array[3:,column] - val_array[0:3,column]))
+        data_list.append(np.std(val_array[3:,column] - val_array[0:3,column]))
+        data_dict[str(column)] = data_list
+        del data_list
+    return pd.DataFrame(data_dict)
+    
+print(_PrPC_PrPSc(numparrFFIoneP1, 0, 3))
+
+###########  get values for plotting ###################
 ##mean_valsPMCA, DNstdPMCA = means_SDs(DN_vals_PMCA)
 ##print("Mean vals PMCA\n=======\n", mean_valsPMCA)
 ##mean_valsPMCA2, DNstdPMCA2 = means_SDs(DN_vals_PMCA2)
 ##print("Mean vals PMCA2\n=======\n", mean_valsPMCA2)
-mean_valsBr, DNstdBr = means_SDs(DN_vals_case1)
-print("Mean vals Br\n=======\n", mean_valsBr)
-mean_valsBr2, DNstdBr2 = means_SDs(DN_vals_case2)
-print("Mean vals Br2\n=======\n", mean_valsBr2)
-mean_valsSCJDFC, DNstdSCJDFC = means_SDs(DN_vals_sCJDMM1FC)
-print("Mean vals sCJD FC\n=======\n", mean_valsSCJDFC)
+##mean_valsBr, DNstdBr = means_SDs(DN_vals_case1)
+##print("Mean vals Br\n=======\n", mean_valsBr)
+##mean_valsBr2, DNstdBr2 = means_SDs(DN_vals_case2)
+##print("Mean vals Br2\n=======\n", mean_valsBr2)
+##mean_valsSCJDFC, DNstdSCJDFC = means_SDs(DN_vals_sCJDMM1FC)
+##print("Mean vals sCJD FC\n=======\n", mean_valsSCJDFC)
 
-mean_list = []
-for arr in DN_nonCJD:
-    mean_vals, _ = means_SDs(arr)
-    print("Mean vals ",str(arr),"\n=======\n", mean_vals)
-    mean_list.append(mean_vals)
-print("All the means\n=======\n", mean_list)
-mean_of_mean_nonCJD = np.vstack(mean_list)
-mean_nonCJD, DNstd_nonCJD = means_SDs(mean_of_mean_nonCJD)
-print("Mean vals nonCJD FC\n=======\n", mean_nonCJD)  
-
-#put above values in list
-mean_val_list = [
-    ["FFI case 1 FC", mean_valsBr, DNstdBr, ('0', '1', '2.5','5', '10', '50')],
-    ["FFI case 2 FC", mean_valsBr2, DNstdBr2, ('0', '1', '2.5','5', '10', '50')],
-    ["sCJD MM1 FC", mean_valsSCJDFC, DNstdSCJDFC, ('0', '1', '2.5','5', '10', '50')],
-    ["Non CJD", mean_nonCJD, DNstd_nonCJD, ('0', '1', '2.5','5', '10', '50')]]
+############# calculate mean of means and std ###################
+##mean_list = []
+##for arr in DN_nonCJD:
+##    mean_vals, _ = means_SDs(arr)
+##    print("Mean vals ",str(arr),"\n=======\n", mean_vals)
+##    mean_list.append(mean_vals)
+##print("All the means\n=======\n", mean_list)
+##mean_of_mean_nonCJD = np.vstack(mean_list)
+##mean_nonCJD, DNstd_nonCJD = means_SDs(mean_of_mean_nonCJD)
+##print("Mean vals nonCJD FC\n=======\n", mean_nonCJD)  
+####put above values in list
+##mean_val_list = [
+##    ["FFI case 1 FC", mean_valsBr, DNstdBr, ('2.5','5', '10', '50')],
+##    ["FFI case 2 FC", mean_valsBr2, DNstdBr2, ('2.5','5', '10', '50')],
+##    ["sCJD MM1 FC", mean_valsSCJDFC, DNstdSCJDFC, ('2.5','5', '10', '50')],
+##    ["Non CJD", mean_nonCJD, DNstd_nonCJD, ('2.5', '10', '50')]]
 
 #from list, plot
 def assembleBar(compnts):
@@ -163,10 +199,9 @@ def assembleBar(compnts):
         plt.subplot((math.ceil(len(compnts)/2)), 2, i+1)
         plotBar(*compnts[i])
 
-assembleBar(mean_val_list)
-
-plt.subplots_adjust(wspace = 0.4)
-plt.subplots_adjust(hspace = 0.6)
-plt.show()
+##assembleBar(mean_val_list)
+##plt.subplots_adjust(wspace = 0.4)
+##plt.subplots_adjust(hspace = 0.6)
+##plt.show()
 
 
